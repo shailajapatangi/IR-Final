8f4a199dbb9f104d811e9f9b6b883a51b9ff2a57
Shailaja Patangi <shailajapatangi@gmail.com>
2020-04-24 20:12:12 -0500

    Changed ui and added index page

--- IR_Climate/Climate_App/views.py
+++ IR_Climate/Climate_App/views.py
@@ -10,40 +10,52 @@ assert subscription_key
 
 search_url = "https://api.cognitive.microsoft.com/bing/v7.0/search"
 search_term=""
+activeTab="custom"
 
 def getClimateData(request):
-    return render(request, 'Climate_App/climate.html')
+    return render(request, 'Climate_App/index.html')
 
 def getGoogleResults(request):
+     global activeTab
      my_api_key = "AIzaSyDbiy21NBP13V3FzvRe1LaF5E0UmwwfuPo"
      my_cse_id = "014871123708314990102:5dujllnfiee"
      service = build("customsearch", "v1", developerKey=my_api_key)
      res = service.cse().list(q=search_term, cx=my_cse_id).execute()
-     return render(request, 'Climate_App/googleResults.html', {"google": json.dumps(res)})
+     activeTab = "google"
+     return render(request, 'Climate_App/googleResults.html', {"google": json.dumps(res), "activeTab" : activeTab, "search_term" : search_term})
     #return render(request, 'Climate_App/googleResults.html')
 
 def getCustomResults(request):
-    return render(request, 'Climate_App/customResults.html')
+    global activeTab
+    activeTab = "custom"
+    return render(request, 'Climate_App/customResults.html', {"activeTab" : activeTab, "search_term" : search_term})
 
 def getBingResults(request):
+    global activeTab
     headers = {"Ocp-Apim-Subscription-Key": subscription_key}
     params = {"q": search_term, "textDecorations": True, "textFormat": "HTML"}
     response = requests.get(search_url, headers=headers, params=params)
     response.raise_for_status()
     search_results = response.json()
     y = json.dumps(search_results)
-    return render(request, 'Climate_App/bingResults.html', {"bing": y})
+    activeTab="bing"
+    return render(request, 'Climate_App/bingResults.html', {"bing": y,"activeTab" : activeTab, "search_term" : search_term})
 
 def getClusterResults(request):
-    return render(request, 'Climate_App/clusterResults.html')
+    global activeTab
+    activeTab="cluster"
+    return render(request, 'Climate_App/clusterResults.html', {"activeTab" : activeTab, "search_term" : search_term})
 
 def getQueryExpansionResults(request):
-    return render(request, 'Climate_App/queryExpansionResults.html')
+    global activeTab
+    activeTab="queryExpansion"
+    return render(request, 'Climate_App/queryExpansionResults.html', {"activeTab" : activeTab, "search_term" : search_term})
 
 
 
 def getSearchQuery(request):
     global search_term
-    search_term = request.GET['search']
-    return render(request, 'Climate_App/climate.html', {"search_term" : search_term})
+    search_term = request.GET['searchResult']
+
+    return render(request, 'Climate_App/index.html', {"search_term" : search_term})
 
